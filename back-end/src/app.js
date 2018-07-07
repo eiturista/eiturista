f86e9141da8449add7d1504648a8044c44bad040
Isaiaszmendes <isaiaszmendes@gmail.com>
2018-07-07 16:11:22 -0300

    add routes para teste da app

--- back-end/src/app.js
+++ back-end/src/app.js
@@ -1,23 +1,23 @@
+'use strict';
+
 const express = require('express');
+const bodyParser = require('body-parser');
 
 const app = express();
 const router = express.Router();
 
-let route = router.get('/', (req, res, next) => {
-	res.status(201).send({
-		title: "API para o Eiturismo",
-		version: "0.0.1"
-	});
-});
+// carrega as Rotas
+const indexRoute = require('./routes/index');
+const userRoute  = require('./routes/user');
 
-let create = router.get('/create', (req, res, next) => {
-	res.status(201).send({
-		title: "Bora criar"
-		// version: "0.0.1"
-	});
-});
+// Todo conteudo é convertido para JSON
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ 
+	extended: false 
+}));
 
-app.use('/', route);
-app.use('/create', create);
+app.use('/', indexRoute);
+app.use('/', userRoute);
+
 
 module.exports = app;
--- back-end/src/database/index.js
+++
@@ -1,7 +0,0 @@
-const mongoose = require('mongoose');
-
-mongoose.connect('mongodb://localhost:27017/db_eiturista', {  useNewUrlParser: true});
-mongoose.Promise = global.Promise;
-
-
-module.exports = mongoose;

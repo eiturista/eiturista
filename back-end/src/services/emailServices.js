d9bfbfa48507c84787dca1770bf8ce494febe1c3
isaias mendes <isaiaszmendes@gmail.com>
2018-07-12 23:36:48 -0300

    template teste para email futuro

--- back-end/src/services/emailServices.js
+++ back-end/src/services/emailServices.js
@@ -2,12 +2,13 @@
 
 let config = require('../config/config.js');
 let sendgrid = require('sendgrid')(config.sendgridKey);
+let template = require('../services/template-email');
 
 exports.send = async (to, subject, body) => {
 	sendgrid.send({
 		to: to,
 		from: 'eituristacontato@gmail.com',
 		subject: subject, 
-		html: body
+		html: template.email_tamplate
 	});
 }
---
+++ back-end/src/services/template-email.js
@@ -0,0 +1,21 @@
+module.exports = {
+  email_tamplate: `
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <title></title>
+  <link rel="stylesheet" href="">
+</head>
+<body>
+    <img src="" alt="">
+    <h1>Lorem ipsum dolor sit amet, consectetuer adipiscing elit.</h1>
+
+    <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim. Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu. In enim justo, rhoncus ut, imperdiet a, venenatis vitae, justo. Nullam dictum felis eu pede mollis pretium.</p>
+
+    <button> Vem com a gente</button>
+</body>
+</html>
+`
+}

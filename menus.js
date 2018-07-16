ae203820041d2cf3ee7a5271c6384971f2362b61
Ana Beatriz <anabemorita@gmail.com>
2018-07-16 12:14:51 -0300

    Implementar clique do menu de usuários

--- menus.js
+++ menus.js
@@ -1,16 +1,50 @@
-const logincadastro = document.querySelector("#usuario");
-const botaoicone = document.querySelector("#icone_perfil");
+const loginCadastro = document.querySelector(".usuario");
+const botaoIcone = document.querySelector(".icone_perfil");
+const botaoLogin = document.querySelector(".logar");
+const botaoCadastro = document.querySelector(".cadastrar");
+const telaCadastrar = document.querySelector(".cadastro");
+const telaLogar = document.querySelector(".login");
 
-function aparecer(){
-        if(logincadastro.classList.contains("active")){
-            logincadastro.classList.remove("active");
-            botaoicone.classList.remove("active");
+
+function aparecerMenu(){
+        if(!botaoIcone.classList.contains("active")){
+            loginCadastro.classList.add("active");
+            botaoIcone.classList.add("active");   
         }
         else{
-            logincadastro.classList.add("active");
-            botaoicone.classList.add("active");
-        
-    }
-    
-    botaoicone.onclick = aparecer;
+            loginCadastro.classList.remove("active");
+            botaoIcone.classList.remove("active");
+            telaCadastrar.classList.remove("active"); 
+            telaLogar.classList.remove("active"); 
+        }
 }
+
+function cadastrar(){
+    if(!telaCadastrar.classList.contains("active")){
+        telaCadastrar.classList.add("active");
+        loginCadastro.classList.remove("active");
+        telaLogar.classList.remove("active");    
+    }
+    console.log("cadastrar");
+    // else{
+    //     loginCadastro.classList.remove("active");
+    //     botaoIcone.classList.remove("active");
+    // }
+}
+
+function logar(){
+    if(!telaLogar.classList.contains("active")){
+        telaLogar.classList.add("active");
+        loginCadastro.classList.remove("active");
+        telaCadastrar.classList.remove("active");    
+    }
+    console.log("logar");
+    // else{
+    //     loginCadastro.classList.remove("active");
+    //     botaoIcone.classList.remove("active");
+    // }
+}
+
+botaoIcone.onclick = aparecerMenu;
+botaoCadastro.onclick = cadastrar;
+botaoLogin.onclick = logar;
